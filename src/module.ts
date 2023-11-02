41aa04208780cce02e28ec95bc2e474969ae6986
Sadjow Leão <sadjow@gmail.com>
2023-11-02 18:19:40 -0300

    feat(ci): return error if lint warns

--- src/module.ts
+++ src/module.ts
@@ -10,7 +10,7 @@ export default defineNuxtModule<ModuleOptions>({
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  setup() {
+  setup(options, nuxt) {
     const resolver = createResolver(import.meta.url);
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
