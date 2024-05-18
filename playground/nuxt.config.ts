f1ca0985b3741b9b6d7266bb85c252de75090a45
Sadjow Leão <sadjow@gmail.com>
2024-05-18 10:20:26 -0300

    chore(playground): add a title to the playground app.

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,4 +1,9 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Nuxt UTM Playground",
+    },
+  },
   modules: ["../src/module"],
   utm: {},
   devtools: { enabled: true },
